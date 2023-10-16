bb77a587cd3b9e8a9b9aae511682fb6279aaaa92
Nikola <nikola.kerleta1@gmail.com>
2023-10-16 16:19:06 +0200

    Handleing possible errors

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,4 +1,5 @@
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 type ResultType = {
   title:string,
@@ -12,8 +13,22 @@ export default async function WebSearchPage({searchParams}:{searchParams:{search
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if(!response.ok){
+    throw new Error("Something went wrong!")
+  }
   const data = await response.json();
+
   const results = data.items;
+
+  if(!results){
+    return <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="text-3xl mb-4">No results found</h1>
+      <p className="text-lg">Try searching for something else or go back to </p>
+      <Link href = "/" className="text-blue-500">
+        HOME
+      </Link>
+    </div>
+  }
 
   return (
     <>
